7448101af5ff043d351628f7b979f1fdd73f898a
Natalia <nataliamirandac@hotmail.com>
2020-08-10 17:17:13 -0400

    Endpoints - CRUD - accounts router - DONE

--- api/server.js
+++ api/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const db = require("../data/dbConfig.js");
 
+const accountRouter = require("../api/account-router.js");
+
 const server = express();
 
-server.use(express.json());
+server.use(express.json()); //<< need to be before the router !!!! At the top level in the file
+
+server.use("/api/accounts", accountRouter);
 
 module.exports = server;
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 5000;
 
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up - server working" });
+});
+
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
 });
